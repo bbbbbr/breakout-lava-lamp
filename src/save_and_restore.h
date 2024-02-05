63bcf72a3b771a572130b0b0f73abbb49b2de07d
bbbbbr <bbbbbr@users.noreply.github.com>
2024-02-05 02:41:15 -0800

    Remove unused include that isn't cross platform (GG)

--- src/save_and_restore.h
+++ src/save_and_restore.h
@@ -1,8 +1,6 @@
 #ifndef _SAVE_AND_RESTORE_H
 #define _SAVE_AND_RESTORE_H
 
-#include <gbdk/bcd.h>
-
 #define SAVE_FRAMES_THRESHOLD  60u  // Save about once per second (if using EEPROM carts make this configurable based on the cart)
 
 void savedata_load(void);
